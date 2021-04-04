c9f4bba3687d7ef156689256cfbece5a535715a9
YeongminKim <oinochoe@naver.com>
2021-04-04 13:18:27 +0900

    스타일 업데이트

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,4 +3,6 @@ require('dotenv').config();
 module.exports = {
     host: new URL(process.env.URL || 'https://localhost').host,
     trello_board: process.env.TRELLO_BOARD_URL || 'https://trello.com/b/BHLrP0F4/noel',
+    tistory: process.env.TISTORY_ADDRESS,
+    github: process.env.GITHUB_ADDRESS,
 };
