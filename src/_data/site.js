2c112cedbe36d70dfe06832b78a8b3b912a757a2
YeongminKim <oinochoe@naver.com>
2021-04-04 17:26:33 +0900

    link default 값 추가

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
     host: new URL(process.env.URL || 'https://localhost').host,
     trello_board: process.env.TRELLO_BOARD_URL || 'https://trello.com/b/BHLrP0F4/noel',
-    tistory: process.env.TISTORY_ADDRESS,
-    github: process.env.GITHUB_ADDRESS,
+    tistory: process.env.TISTORY_ADDRESS || 'https://frontdev.tistory.com/',
+    github: process.env.GITHUB_ADDRESS || 'https://github.com/oinochoe/connect-Trello',
 };
